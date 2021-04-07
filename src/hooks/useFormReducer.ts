be1f3951c01318bbdf580c8477caea7fdae6550a
Tai <4382091+taisukemino@users.noreply.github.com>
2021-04-07 12:53:48 +0900

    feat: add the useFormReducer

---
+++ src/hooks/useFormReducer.ts
@@ -0,0 +1,64 @@
+import { useReducer } from "react";
+
+export function useFormReducer() {
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "CHOOSE_ETH": {
+          return {
+            ...state,
+            underlyingAsset: "ETH"
+          };
+        }
+        case "CHOOSE_WBTC": {
+          return {
+            ...state,
+            underlyingAsset: "WBTC"
+          };
+        }
+        case "CHOOSE_CALL": {
+          return {
+            ...state,
+            optionType: "Call"
+          };
+        }
+        case "CHOOSE_PUT": {
+          return {
+            ...state,
+            optionType: "Put"
+          };
+        }
+        case "UPDATE_EXPIRATION_DATE": {
+          return {
+            ...state,
+            expirationDate: action.value
+          };
+        }
+        case "UPDATE_STRIKE_PRICE": {
+          return {
+            ...state,
+            strikePrice: action.value
+          };
+        }
+        case "UPDATE_OPTION_SIZE": {
+          return {
+            ...state,
+            optionSize: action.value
+          };
+        }
+        default: {
+          return state;
+        }
+      }
+    },
+    {
+      underlyingAsset: "ETH",
+      optionType: "Call",
+      expirationDate: "",
+      strikePrice: 0,
+      optionSize: 0
+    }
+  );
+
+  return [state, dispatch];
+}
