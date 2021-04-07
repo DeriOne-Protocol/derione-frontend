c9b42c19aff4e88ef2f0543696030930c0536917
Tai <4382091+taisukemino@users.noreply.github.com>
2021-04-07 15:38:18 +0900

    feat: move provider wrappers to the App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
+import { ethers } from "ethers";
+import { Web3ReactProvider } from "@web3-react/core";
 import { Connectors } from "./components/Connectors";
 import { Feed } from "./components/Feed";
 import { Footer } from "./components/Footer";
 import { Forms } from "./components/Forms";
 import { Header } from "./components/Header";
+import { FormContext } from "./contexts/FormContext";
+import { useFormReducer } from "./hooks/useFormReducer";
+
+function getLibrary(provider: any): ethers.providers.Web3Provider {
+  const library = new ethers.providers.Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
+}
 
 export default function App() {
+  const [state, dispatch] = useFormReducer();
+
   return (
     <>
-      <Header />
-      <Forms />
-      <Feed />
-      <Connectors />
-      <Footer />
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <FormContext.Provider value={[state, dispatch]}>
+          <Header />
+          <Forms />
+          <Feed />
+          <Connectors />
+          <Footer />
+        </FormContext.Provider>
+      </Web3ReactProvider>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,10 @@
-import { ethers } from "ethers";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Web3ReactProvider } from "@web3-react/core";
 import App from "./App";
-import { FormProvider } from "./contexts/FormContext";
-
-function getLibrary(provider: any): ethers.providers.Web3Provider {
-  const library = new ethers.providers.Web3Provider(provider);
-  library.pollingInterval = 12000;
-  return library;
-}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <FormProvider>
-        <App />
-      </FormProvider>
-    </Web3ReactProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
