de90821aa4ae450ddb5b6266e8c5ffb353fe1f9d
Tai <4382091+taisukemino@users.noreply.github.com>
2021-04-07 12:46:45 +0900

    feat: wrap the app component with web3 and form providers

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
+import { ethers } from "ethers";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Web3ReactProvider } from "@web3-react/core";
 import App from "./App";
+import { FormProvider } from "./contexts/FormContext";
+
+function getLibrary(provider: any): ethers.providers.Web3Provider {
+  const library = new ethers.providers.Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <FormProvider>
+        <App />
+      </FormProvider>
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
