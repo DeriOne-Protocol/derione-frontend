3982d5b5b1149545acf6edc68a40e2e2dd71ba27
Tai <4382091+taisukemino@users.noreply.github.com>
2021-04-09 08:18:36 +0900

    refactor: move some variables and functions from the connectors component to the connectors util

--- src/utils/connectors.ts
+++ src/utils/connectors.ts
@@ -1,5 +1,13 @@
-import { InjectedConnector } from "@web3-react/injected-connector";
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { UnsupportedChainIdError } from "@web3-react/core";
+import {
+  InjectedConnector,
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected
+} from "@web3-react/injected-connector";
+import {
+  UserRejectedRequestError as UserRejectedRequestErrorWalletConnect,
+  WalletConnectConnector
+} from "@web3-react/walletconnect-connector";
 
 export const injected = new InjectedConnector({
   supportedChainIds: [1, 31337]
@@ -17,3 +25,29 @@ export const walletconnect = new WalletConnectConnector({
   qrcode: true,
   pollingInterval: POLLING_INTERVAL
 });
+
+export enum ConnectorNames {
+  Injected = "MetaMask", // could this be other than MetaMask? Dapper maybe?
+  WalletConnect = "WalletConnect"
+}
+
+export const connectorsByName: { [connectorName in ConnectorNames]: any } = {
+  [ConnectorNames.Injected]: injected,
+  [ConnectorNames.WalletConnect]: walletconnect
+};
+
+export function getErrorMessage(error: Error) {
+  if (error instanceof NoEthereumProviderError) {
+    return "No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.";
+  } else if (error instanceof UnsupportedChainIdError) {
+    return "You're connected to an unsupported network.";
+  } else if (
+    error instanceof UserRejectedRequestErrorInjected ||
+    error instanceof UserRejectedRequestErrorWalletConnect
+  ) {
+    return "Please authorize this website to access your Ethereum account.";
+  } else {
+    console.error(error);
+    return "An unknown error occurred. Check the console for more details.";
+  }
+}
