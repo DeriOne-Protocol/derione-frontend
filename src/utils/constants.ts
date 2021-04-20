f3fc5fa6f25f5c351e8fd42b816e19f28abe6deb
Tai <4382091+taisukemino@users.noreply.github.com>
2021-04-20 15:57:27 +0900

    feat: make a file for constant values

---
+++ src/utils/constants.ts
@@ -0,0 +1,17 @@
+export const ASSETS = {
+  Invalid: 0,
+  DAI: 1,
+  ETH: 2,
+  SUSHI: 3,
+  UNI: 4,
+  USDC: 5,
+  WBTC: 6,
+  WETH: 7,
+  YFI: 8
+};
+
+export const OPTION_TYPES = {
+  Invalid: 0,
+  Put: 1,
+  Call: 2
+};
