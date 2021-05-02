642a731be5ed1092e00ab35c9581bf2ce0c5ba8a
Tai <4382091+taisukemino@users.noreply.github.com>
2021-05-02 15:23:22 +0900

    feat: add new constant values

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const ASSETS = {
+export const ASSET_NAMES = {
   Invalid: 0,
   DAI: 1,
   ETH: 2,
@@ -10,8 +10,61 @@ export const ASSETS = {
   YFI: 8
 };
 
-export const OPTION_TYPES = {
+export const OPTION_TYPE_NAMES = {
   Invalid: 0,
   Put: 1,
   Call: 2
 };
+
+export const ASSET_NUMBERS = {
+  0: "Invalid",
+  1: "DAI",
+  2: "ETH",
+  3: "SUSHI",
+  4: "UNI",
+  5: "USDC",
+  6: "WBTC",
+  7: "WETH",
+  8: "YFI"
+};
+
+export const OPTION_TYPE_NUMBERS = {
+  0: "Invalid",
+  1: "Put",
+  2: "Call"
+};
+
+export const PROTOCOL_NUMBERS = {
+  0: "Invalid",
+  1: "CharmV02",
+  2: "HegicV888",
+  3: "SirenV1"
+};
+
+export const TABLE_COLUMNS = [
+  {
+    name: "Premium",
+    selector: "premium",
+    sortable: true
+  },
+  {
+    name: "Expiration Date",
+    selector: "expirationDate",
+    sortable: true
+  },
+  {
+    name: "Strike Price",
+    selector: "strikePrice",
+    sortable: true
+  },
+  {
+    name: "payment asset",
+    selector: "paymentAsset",
+    sortable: false
+  },
+  {
+    name: "Protocol",
+    selector: "protocol",
+    sortable: false
+  }
+];
